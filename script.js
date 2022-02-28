aaff110e26602a509726dc24d6f6163a1149cf91
Mathieu Symphorien <mathieu.symphorien@etu.univ-amu.fr>
2022-02-28 09:32:26 +0100

    Travail de Seignobos Louis et Symphorien Mathieu, affichage du Min et du max à l'aide de deux fonctions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var minIn;
+var maxIn;
+var minOut;
+var maxOut;
+
 /* Button */
 
 O_optionsBar = document.getElementById('options');
@@ -29,6 +34,8 @@ function getOutdoorSensorValue() {
 	fetch("https://hothothot.dog/api/capteurs/exterieur").then(function(data) {
 		data.json().then(function(json) { 
 			alertOutdoorSensors(json.capteurs[0].Valeur);
+			findOutdoorMin(json.capteurs[0].Valeur);
+			findOutdoorMax(json.capteurs[0].Valeur);
 		});	
 	});
 	console.log("REFRESH_OUT");
@@ -38,6 +45,8 @@ function getIndoorSensorValue() {
 	fetch("https://hothothot.dog/api/capteurs/interieur").then(function(data) {
 		data.json().then(function(json) {
 			alertIndoorSensors(json.capteurs[0].Valeur);
+			findIndoorMin(json.capteurs[0].Valeur);
+			findIndoorMax(json.capteurs[0].Valeur);
 		});	
 	});
 	console.log("REFRESH_IN");
@@ -76,3 +85,37 @@ const timer = 60000;
 
 outdoorInterval = setInterval(getOutdoorSensorValue, timer);
 indoorInterval = setInterval(getIndoorSensorValue, timer);
+
+
+/* Find Min / Max*/
+function findIndoorMin(val) {
+	if(minIn == null)
+		minIn = val;
+	if(val < minIn)
+		minIn = val;
+	document.getElementById("MinTempIndoor").innerText = "Le min à l'intérieur est : " + minIn;
+}
+
+function findIndoorMax(val) {
+	if(maxIn == null)
+		maxIn = val;
+	if(val > maxIn)
+		maxIn = val;
+	document.getElementById("MaxTempIndoor").innerText = "Le max à l'intérieur est : " + maxIn;
+}
+
+function findOutdoorMin(val) {
+	if(minOut == null)
+		minOut = val;
+	if(val < minOut)
+		minOut = val;
+	document.getElementById("MinTempOutdoor").innerText = "Le min à l'exterieur est : " + minOut;
+}
+
+function findOutdoorMax(val) {
+	if(maxOut == null)
+		maxOut = val;
+	if(val > maxOut)
+		maxOut = val;
+	document.getElementById("MaxTempOutdoor").innerText = "Le max à l'exterieur est : " + maxOut;
+}
