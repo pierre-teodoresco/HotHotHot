7249825486ca04a433fd0766fa347f17e6c314cd
pierre <pierre.teodoresco@etu.univ-amu.fr>
2022-02-28 09:18:21 +0100

    Nicolas + Pierre = Historique dynamique + template html

--- script.js
+++ script.js
@@ -23,12 +23,24 @@ O_optionsBar.querySelectorAll('a').forEach(function (element) {
 // 	alert("[message] : " + event.data);
 // };
 
+/* class Temperature */
+
+class Temperature {
+	constructor(val, sensor, date) {
+		this.val = val;
+		this.sensor = sensor;
+		this.date = date;
+	}
+}
+
 /* Récupération des données du serveur avec fetch en attendant l'ouverture pour websocket */
 
 function getOutdoorSensorValue() {
 	fetch("https://hothothot.dog/api/capteurs/exterieur").then(function(data) {
-		data.json().then(function(json) { 
-			alertOutdoorSensors(json.capteurs[0].Valeur);
+		data.json().then(function(json) {
+			let val = json.capteurs[0].Valeur;
+			alertOutdoorSensors(val);
+			addEntryToHistory(new Temperature(val, "Exterieur", new Date().toLocaleString()));
 		});	
 	});
 	console.log("REFRESH_OUT");
@@ -37,14 +49,15 @@ function getOutdoorSensorValue() {
 function getIndoorSensorValue() {
 	fetch("https://hothothot.dog/api/capteurs/interieur").then(function(data) {
 		data.json().then(function(json) {
-			alertIndoorSensors(json.capteurs[0].Valeur);
+			let val = json.capteurs[0].Valeur;
+			alertIndoorSensors(val);
+			addEntryToHistory(new Temperature(val, "Intérieur", new Date().toLocaleString()));
 		});	
 	});
 	console.log("REFRESH_IN");
 }
 
 function alertIndoorSensors(val) {
-
 	if (val < 0) {
 		alert("Canalisations gelées, appelez SOS plombier et mettez un bonnet !");
 	} else if (val < 12) {
@@ -54,20 +67,34 @@ function alertIndoorSensors(val) {
 	} else if (val > 50) {
 		alert("Appelez les pompiers ou arrêtez votre barbecue !");
 	}
-
 	document.getElementById("in-temp").innerText = val;
 }
 
 function alertOutdoorSensors(val) {
-
 	if (val < 0) {
 		alert("Banquise en vue !");
 	} else if (val > 35) {
 		alert("Hot Hot Hot !");
 	}
-
 	document.getElementById("out-temp").innerText = val;
 }
+
+/* Gestion Historique */
+
+function addEntryToHistory(temp) {
+	let template = document.getElementById('entry-template');
+	let clonedRow = document.importNode(template.content, true);
+
+	let cells = clonedRow.querySelectorAll('td');
+	cells[0].innerText = temp.val + '°C';
+	cells[1].innerText = temp.sensor;
+	cells[2].innerText = temp.date;
+
+	let tableBody = document.querySelector('#historic table tbody');
+	tableBody.append(clonedRow);
+}
+
+/* main */
 
 getIndoorSensorValue();
 getOutdoorSensorValue();
