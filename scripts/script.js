beda483a73fa016d317413602903e49e071d7be3
Mathieu Symphorien <mathieu.symphorien@etu.univ-amu.fr>
2022-02-28 12:10:32 +0100

    actualisation v2

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,13 @@
 
 /* main */
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/HotHotHot/service-worker.js')
+        .then(() => { console.log('Service Worker Registered'); });
+
+}
+
 getIndoorSensorValue();
 getOutdoorSensorValue();
 
