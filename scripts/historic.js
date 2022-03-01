0eba1cb339aa4598730c79993d2b84bbb1d39edf
pierre-teodoresco <pierre.teodoresco@etu.univ-amu.fr>
2022-03-01 11:10:28 +0100

    ça marche pas...

--- scripts/historic.js
+++ scripts/historic.js
@@ -5,7 +5,7 @@ function addEntryToHistory(temp) {
     let cells = clonedRow.querySelectorAll('td');
     cells[0].innerText = temp.val + '°C';
     cells[1].innerText = temp.sensor;
-    cells[2].innerText = temp.date;
+    cells[2].innerText = temp.date.toLocaleString();
 
     let tableBody = document.querySelector('#historic table tbody');
     tableBody.append(clonedRow);
@@ -20,38 +20,46 @@ function sortHandler() {
         if (state === "date-up") {
             document.getElementById("svg-date").src = "../images/historic/caret-down.svg";
             state = "date-down";
+            sortTempArrayByDate(getTempArray(), true);
         } else {
             resetSvg();
             document.getElementById("svg-date").src = "../images/historic/caret-up.svg";
             state = "date-up";
+            sortTempArrayByDate(getTempArray(), false);
+            console.log(getTempArray());
         }
     }
     document.getElementById("link-lieu-sort").onclick = function() {
-        if (state === "lieu-up") {
-            document.getElementById("svg-lieu").src = "../images/historic/caret-down.svg";
-            state = "lieu-down";
+        if (state === "sensor-up") {
+            document.getElementById("svg-sensor").src = "../images/historic/caret-down.svg";
+            state = "sensor-down";
+            sortTempArrayBySensor(getTempArray(), true);
         } else {
             resetSvg();
-            document.getElementById("svg-lieu").src = "../images/historic/caret-up.svg";
-            state = "lieu-up";
+            document.getElementById("svg-sensor").src = "../images/historic/caret-up.svg";
+            state = "sensor-up";
+            sortTempArrayBySensor(getTempArray(), false);
+            console.log(getTempArray());
         }
     }
     document.getElementById("link-temp-sort").onclick = function() {
         if (state === "temp-up") {
             document.getElementById("svg-temp").src = "../images/historic/caret-down.svg";
             state = "temp-down";
+            sortTempArrayByTemp(getTempArray(), true);
         } else {
             resetSvg();
             document.getElementById("svg-temp").src = "../images/historic/caret-up.svg";
             state = "temp-up";
-            getTempArray();
+            sortTempArrayByTemp(getTempArray(), false);
+            console.log(getTempArray());
         }
     }
 }
 
 function resetSvg() {
     document.getElementById("svg-temp").src = "";
-    document.getElementById("svg-lieu").src = "";
+    document.getElementById("svg-sensor").src = "";
     document.getElementById("svg-date").src = "";
 }
 
@@ -59,7 +67,7 @@ function getTempArray() {
     let values = [];
     let table = document.querySelectorAll('table tbody tr td');
     let cpt = 0;
-    let val, lieu, date;
+    let val, sensor, date;
     for (let i = 3; i < table.length; ++i) {
         if (cpt === 1) {
             val = table[i].innerHTML;
@@ -71,14 +79,69 @@ function getTempArray() {
             val = parseFloat(val);
             ++cpt;
         } else if (cpt === 1) {
-            lieu = table[i].innerHTML;
+            sensor = table[i].innerHTML;
         } else if (cpt === 2) {
-            date = table[i].innerHTML;
+            date = new Date();
+            date.parse(table[i].innerHTML);
         } else {
-            values.push(new Temperature(val, lieu, date));
+            values.push(new Temperature(val, sensor, date));
             cpt = 0;
         }
     }
     console.log(values);
     return values;
+}
+
+function swap(a, b) {
+    let c = a;
+    a = b;
+    b = c;
+}
+
+function sortTempArrayByTemp(tempArray, ascending) {
+    if (ascending) {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i].val > tempArray[i + 1].val) {
+                swap(tempArray[i].val, tempArray[i + 1].val)
+            }
+        }
+    } else {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i].val < tempArray[i + 1].val) {
+                swap(tempArray[i], tempArray[i + 1])
+            }
+        }
+    }
+}
+
+function sortTempArrayBySensor(tempArray, ascending) {
+    if (ascending) {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i].sensor === 'Intérieur' && tempArray[i + 1].sensor === 'Extérieur') {
+                swap(tempArray[i], tempArray[i + 1])
+            }
+        }
+    } else {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i + 1].sensor === 'Intérieur' && tempArray[i].sensor === 'Extérieur') {
+                swap(tempArray[i], tempArray[i + 1])
+            }
+        }
+    }
+}
+
+function sortTempArrayByDate(tempArray, ascending) {
+    if (ascending) {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i].date < tempArray[i + 1].date) {
+                swap(tempArray[i], tempArray[i + 1]);
+            }
+        }
+    } else {
+        for (let i = 0; i < tempArray.length; ++i) {
+            if (tempArray[i].date < tempArray[i + 1].date) {
+                swap(tempArray[i], tempArray[i + 1]);
+            }
+        }
+    }
 }
