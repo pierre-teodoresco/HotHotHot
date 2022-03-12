a5dda56f1d70f5e93708e47f4206d2c8854031fe
MathieuSymphorien <mathieu.symphorien@etu.univ-amu.fr>
2022-03-12 12:27:15 +0100

    Stockage dans le cache

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -2,6 +2,18 @@
     fetch("https://hothothot.dog/api/capteurs/exterieur").then(function(data) {
         data.json().then(function(json) {
             let val = json.capteurs[0].Valeur;
+            let obj;
+            if (!!localStorage.getItem("Temperature"))
+                localStorage.setItem("Temperature", '[]');
+            else {
+                //stockage
+
+
+                JSON.parse(localStorage.getItem("Temperature", val);
+                obj = JSON.parse(json);
+                console.log(json);
+                console.log(obj);
+            }
             alertOutdoorSensors(val);
             addEntryToHistory(new Temperature(val, "Extérieur", new Date()), true);
         });
