66ac28927b0369b5dc189bc359275c31fcd268bd
Martin Moreira de Jesus <martin.moreira-de-jesus@etu.univ-amu.fr>
2022-03-24 22:55:44 +0100

    implemented more methods in observer subject design pattern

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import AppNavigation from "./lib/Observers/AppNavigation.js";
 import NavBar from "./lib/Subjects/NavBar.js";
+import Sensors from "./lib/Subjects/Sensors.js";
+import SensorsDisplay from "./lib/Observers/SensorsDisplay.js"
+import HistoricDisplay from "./lib/Observers/HistoricDisplay.js"
 
+/*
+    Subjects
+ */
 const navBar = new NavBar();
-const navigation = new AppNavigation();
+const sensors = new Sensors();
 
+/*
+    Observers
+ */
+const navigation = new AppNavigation();
+const sensorsDisplay = new SensorsDisplay();
+const historicDisplay = new HistoricDisplay();
+
+/*
+ * Binding stuff
+ */
 navBar.attach(navigation);
+sensors.attach(sensorsDisplay)
+sensors.attach(historicDisplay);
