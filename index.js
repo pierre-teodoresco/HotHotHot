39c7189b490f443a370ce479c205c51a06a731d2
martin <martin.moreira-de-jesus@etu.univ-amu.fr>
2022-03-29 23:39:00 +0200

    pagination implemented

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ import Sensors from "./lib/Subjects/Sensors.js";
 import SensorsDisplay from "./lib/Observers/SensorsDisplay.js"
 import HistoricDisplay from "./lib/Observers/HistoricDisplay.js"
 import Pagination from "./lib/Observers/Pagination.js"
+import HistoricPageButtons from "./lib/Subjects/HistoricPageButtons.js";
 
 /*
     Subjects
  */
 const navBar = new NavBar();
 const sensors = new Sensors();
-
+const historicPageButtons = new HistoricPageButtons();
 /*
     Observers
  */
 const navigation = new AppNavigation();
 const sensorsDisplay = new SensorsDisplay();
 const historicDisplay = new HistoricDisplay();
-const pagination = new Pagination();
+const pagination = new Pagination(historicPageButtons);
 /*
  * Binding stuff
  */
 navBar.attach(navigation);
+historicPageButtons.attach(pagination);
 sensors.attach(sensorsDisplay);
 sensors.attach(historicDisplay);
 sensors.attach(pagination);
