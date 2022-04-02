81fe64a9cb04d7e2146db868d23b76808a3bf373
martin <martin.moreira-de-jesus@etu.univ-amu.fr>
2022-04-02 18:02:53 +0200

    pagination fixed and started implementing sorting

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import SensorsDisplay from "./lib/Observers/SensorsDisplay.js"
 import HistoricDisplay from "./lib/Observers/HistoricDisplay.js"
 import Pagination from "./lib/Observers/Pagination.js"
 import HistoricPageButtons from "./lib/Subjects/HistoricPageButtons.js";
+import Sorter from "./lib/Observers/Sorter.js";
+import SortButtons from "./lib/Subjects/SortButtons.js";
 
 /*
     Subjects
@@ -12,18 +14,25 @@ import HistoricPageButtons from "./lib/Subjects/HistoricPageButtons.js";
 const navBar = new NavBar();
 const sensors = new Sensors();
 const historicPageButtons = new HistoricPageButtons();
+const sortButtons = new SortButtons();
 /*
     Observers
  */
 const navigation = new AppNavigation();
 const sensorsDisplay = new SensorsDisplay();
 const historicDisplay = new HistoricDisplay();
-const pagination = new Pagination(historicPageButtons);
+const pagination = new Pagination();
+const sorter = new Sorter();
+
 /*
  * Binding stuff
  */
 navBar.attach(navigation);
+
 historicPageButtons.attach(pagination);
+
 sensors.attach(sensorsDisplay);
 sensors.attach(historicDisplay);
 sensors.attach(pagination);
+
+sortButtons.attach(sorter);
